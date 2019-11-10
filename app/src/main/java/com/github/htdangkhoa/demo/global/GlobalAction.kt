4435313b902f50b0314b5ae77cefa1aca273053e
Huỳnh Trần Đăng Khoa <huynhtran.dangkhoa@gmail.com>
2019-11-11 01:16:50 +0700

    Update GlobalAction.kt

--- app/src/main/java/com/github/htdangkhoa/demo/global/GlobalAction.kt
+++ app/src/main/java/com/github/htdangkhoa/demo/global/GlobalAction.kt
@@ -6,7 +6,7 @@ sealed class GlobalAction: Action {
     data class IS_LOADING(val payload: Boolean = false): GlobalAction()
 
     companion object {
-        fun updateLoadingAction(isLoading: Boolean, dispatch: (action: com.github.htdangkhoa.kdux.Action) -> Unit) {
+        fun updateLoadingAction(isLoading: Boolean, dispatch: (action: Action) -> Unit) {
             return dispatch(IS_LOADING(isLoading))
         }
     }
